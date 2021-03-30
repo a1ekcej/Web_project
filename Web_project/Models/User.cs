33254309058ade19a0e54b258cb6f57bf2ccf0e9
Alexey Yuriev <a1ekcej@yandex.ru>
2021-03-30 18:54:51 +0300

    refresh Layout delete extra files

---
+++ Web_project/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web_project.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string NickName { get; set; }
+        public string Message { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
