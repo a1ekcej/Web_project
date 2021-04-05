9f6b059dcdd73f9f2a9ed660f4d8b2f19b39b77b
a1ekcej <a1ekcej@yandex.ru>
2021-04-05 04:42:06 +0300

    qwerty

--- Web_project/Models/User.cs
+++ Web_project/Models/User.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Text.Json;
 
 namespace Web_project.Models
 {
+    
     public class User
     {
         public int Id { get; set; }
